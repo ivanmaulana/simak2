f9da4df0f9e80b5b71ad3fb7180c932a52c42a8f
ivanmaulana <ivanmaulanaputra@gmail.com>
2016-07-18 18:35:49 +0700

    edit pendaftaran skripsi

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,4 @@
 import {Component, ViewEncapsulation} from '@angular/core';
-import {CKEditor} from 'ng2-ckeditor';
 
 import {PopularApp} from './popularApp';
 import {PieChart} from './pieChart';
@@ -10,36 +9,17 @@ import {Feed} from './feed';
 import {Todo} from './todo';
 import {Calendar} from './calendar';
 import {BaCard} from '../../theme/components';
+import { SEMANTIC_COMPONENTS, SEMANTIC_DIRECTIVES } from 'ng-semantic';
 
 @Component({
   selector: 'dashboard',
   pipes: [],
-  directives: [CKEditor, PopularApp, PieChart, TrafficChart, UsersMap, LineChart, Feed, Todo, Calendar, BaCard],
-  encapsulation: ViewEncapsulation.None,
+  directives: [PopularApp, PieChart, TrafficChart, UsersMap, LineChart, Feed, Todo, Calendar, BaCard, SEMANTIC_COMPONENTS, SEMANTIC_DIRECTIVES],
   styles: [require('./dashboard.scss')],
   template: require('./dashboard.html')
 })
 export class Dashboard {
-  ckeditorContent;
-
-  constructor() {
-    this.ckeditorContent = "<p>dwdwadw</p>";
-  }
-
-}
-
-@Component({
-  selector: 'ckeditor',
-  directives: [CKEditor, BaCard]
-})
-
-export class Ckeditor {
-  public ckeditorContent:string = '<p>Hello CKEditor</p>';
-  public config = {
-    uiColor: '#F0F3F4',
-    height: '600'
-  };
-
   constructor() {
   }
+
 }
