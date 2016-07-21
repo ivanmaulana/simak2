7a3b8b24144d2385c0ee348ad11515e09d87e7cc
ivanmaulana <ivanmaulanaputra@gmail.com>
2016-07-21 15:48:23 +0700

    edit profile page

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -2,11 +2,12 @@ import {Component, ViewEncapsulation} from '@angular/core';
 
 import {BaCard} from '../../theme/components';
 import {BaAppPicturePipe} from '../../theme/pipes';
+import {BaPictureUploader} from '../../theme/components';
 
 @Component({
   selector: 'profile',
   pipes: [BaAppPicturePipe],
-  directives: [BaCard],
+  directives: [BaCard, BaPictureUploader],
   encapsulation: ViewEncapsulation.None,
   styles: [require('./profile.scss')],
   template: require('./profile.html')
@@ -18,6 +19,14 @@ export class Profile {
   private nama: string = "Ivan Maulana Putra";
   private nim: string = "G64130076";
   private no: string = "08988918895";
+
+  public defaultPicture = 'assets/img/theme/no-photo.png';
+  public profile:any = {
+    picture: 'assets/img/app/profile/Nasta.png'
+  };
+  public uploaderOptions:any = {
+    // url: 'http://website.com/upload'
+  };
 
   constructor() {
   }
