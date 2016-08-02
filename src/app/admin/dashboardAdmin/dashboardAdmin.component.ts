63f2f945bc1af3a42a4980d94891ea8af007a4a1
ivanmaulana <ivanmaulanaputra@gmail.com>
2016-08-02 18:40:24 +0700

    update pengajuanAdmin page

--- src/app/admin/dashboardAdmin/dashboardAdmin.component.ts
+++ src/app/admin/dashboardAdmin/dashboardAdmin.component.ts
@@ -1,6 +1,8 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import {Component, ViewEncapsulation, OnInit} from '@angular/core';
 
 import {BaCard} from '../../theme/components';
+import {Http, Headers} from '@angular/http';
+
 
 @Component({
   selector: 'dashboardAdmin',
@@ -10,9 +12,20 @@ import {BaCard} from '../../theme/components';
   styles: [require('./dashboardAdmin.scss')],
   template: require('./dashboardAdmin.html')
 })
-export class DashboardAdmin {
-  
-  constructor() {
+
+export class DashboardAdmin implements OnInit {
+  response;
+
+  ngOnInit(){
+    this.http.get('http://localhost:8000/ta/daftar')
+      .map(res => res.json())
+      .subscribe(data => {
+        this.response = data;
+      })
+  }
+
+  constructor(private http: Http) {
+
   }
 
 }
--- src/app/admin/pengajuanAdmin/pengajuanAdmin.component.ts
+++ src/app/admin/pengajuanAdmin/pengajuanAdmin.component.ts
@@ -1,18 +1,32 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import {Component, ViewEncapsulation, OnInit} from '@angular/core';
+import {Http, Headers} from '@angular/http';
 
 import {BaCard} from '../../theme/components';
+import {BaAppPicturePipe} from '../../theme/pipes';
 
 @Component({
   selector: 'pengajuanAdmin',
-  pipes: [],
+  pipes: [BaAppPicturePipe],
   directives: [BaCard],
   encapsulation: ViewEncapsulation.None,
   styles: [require('./pengajuanAdmin.scss')],
   template: require('./pengajuanAdmin.html')
 })
-export class PengajuanAdmin {
+export class PengajuanAdmin implements OnInit {
+  response;
 
-  constructor() {
+  ngOnInit(){
+    this.http.get('http://localhost:8000/ta/daftar')
+      .map(res => res.json())
+      .subscribe(data => {
+        this.response = data;
+      })
+  }
+
+  peopleTableData:Array<any>;
+
+  constructor(private http: Http) {
+
   }
 
 }
