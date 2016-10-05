a969a5e18c46122316be9c70ac5ed671cc30c147
ivanmaulana <ivanmaulanaputra@gmail.com>
2016-10-06 06:02:01 +0700

    update log dosen

--- src/app/dosen/LogDosen/logDosen.component.ts
+++ src/app/dosen/LogDosen/logDosen.component.ts
@@ -2,6 +2,7 @@ import {Component, ViewEncapsulation, OnInit} from '@angular/core';
 
 import {BaCard} from '../../theme/components';
 import {Http, Headers} from '@angular/http';
+import {AuthHttp} from 'angular2-jwt';
 
 import {Router} from '@angular/router-deprecated';
 
@@ -40,14 +41,12 @@ export class LogDosen implements OnInit {
 
   }
 
-  constructor(private http: Http, private router: Router) {
+  constructor(private http: Http, private authHttp: AuthHttp) {
     this.getDataLog();
   }
 
   getDataLog(){
-    this.dosen_id = localStorage.getItem('id');
-
-    this.http.get('http://210.16.120.17:8000/log/dosen/'+this.dosen_id)
+    this.authHttp.get('http://simak.apps.cs.ipb.ac.id:2016/log/dosen/')
       .map(res => res.json())
       .subscribe(data => {
         this.resLog = data;
@@ -57,14 +56,14 @@ export class LogDosen implements OnInit {
   openBimbingan(logId){
     this.id = logId;
 
-    this.http.get('http://210.16.120.17:8000/log/detail/'+logId)
+    this.authHttp.get('http://simak.apps.cs.ipb.ac.id:2016/log/detail/'+logId)
       .map(res => res.json())
       .subscribe(data => {
         this.resDetail = data;
         if (this.resDetail) this.res = 1;
       })
 
-    this.http.get('http://210.16.120.17:8000/log/jawaban/'+logId)
+    this.authHttp.get('http://simak.apps.cs.ipb.ac.id:2016/log/jawaban/'+logId)
       .map(res => res.json())
       .subscribe(data => {
         this.resJawaban = data;
@@ -72,11 +71,9 @@ export class LogDosen implements OnInit {
   }
 
   balas(id){
-    let headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-    this.creds = JSON.stringify({topikId: id, dosen_id: this.dosen_id, jawaban: this.balasan});
+    this.creds = JSON.stringify({topikId: id, jawaban: this.balasan});
 
-    this.http.post("http://210.16.120.17:8000/log/balas/dosen/", this.creds, {headers: headers})
+    this.authHttp.post("http://simak.apps.cs.ipb.ac.id:2016/log/balas/dosen/", this.creds)
       .map(res => res.json())
       .subscribe(data => {
         this.status = data['status'];
@@ -87,6 +84,33 @@ export class LogDosen implements OnInit {
     )
 
     this.balasan = "";
+  }
+
+
+
+  ubahTanggal(date){
+    let tahun = date.substr(0,4);
+    let bulan = date.substr(5,2);
+    let tanggal = date.substr(8,2);
+
+    if (tanggal.substr(0,1) == 0) tanggal = tanggal.substr(1,1);
+
+    if (bulan == 1) bulan = 'Januari';
+    else if (bulan == 2) bulan = 'Februari';
+    else if (bulan == 3) bulan = 'Maret';
+    else if (bulan == 4) bulan = 'April';
+    else if (bulan == 5) bulan = 'Mei';
+    else if (bulan == 6) bulan = 'Juni';
+    else if (bulan == 7) bulan = 'Juli';
+    else if (bulan == 8) bulan = 'Agustus';
+    else if (bulan == 9) bulan = 'September';
+    else if (bulan == 10) bulan = 'Oktober';
+    else if (bulan == 11) bulan = 'November';
+    else if (bulan == 12) bulan = 'Desember';
+
+    let kirim = tanggal+' '+bulan+' '+tahun;
+
+    return kirim;
   }
 
 }
